a98cdb5e269d81f7e40bd207b3854e8fc8e7808f
meyori <meyori@DESKTOP-C70V8SN>
2024-11-05 21:08:26 +0200

    Add project files.

---
+++ WebApplication2/Controllers/SurveyDBController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models.Entities;
+
+namespace WebApplication2.Controllers
+{
+    public class Lab2Controller : Controller
+    {
+        public ActionResult ListOfPeople()
+        {
+            List<Person> people = new List<Person>();
+            using (var db = new SurveyDB1Entities())
+            {
+                people = db.People.OrderByDescending(x => x.Age)
+                    .ThenBy(x => x.LastName)
+                    .ThenBy(x => x.FirstName).ToList();
+            }
+            return View(people);
+        }
+        [HttpGet]
+        public ActionResult PersonDetails(Guid personId)
+        {
+            Person model = new Person();
+            using (var db = new SurveyDB1Entities())
+            {
+                model = db.People.Find(personId);
+            }
+            return View(model);
+        }
+
+    }
+}
